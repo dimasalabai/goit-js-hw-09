3c62f5e1bad94432eda39b6c05361709b7885e5b
dimasalabai <113694939+dimasalabai@users.noreply.github.com>
2023-11-15 20:22:16 +0000

    Deploying to gh-pages from @ dimasalabai/goit-js-hw-09@87eb02e8b5c3b90082cf92cfd72a55f543f7428f 🚀

---
+++ 01-color-switcher.30adb52e.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("[data-start]"),stop:document.querySelector("[data-stop]"),body:document.querySelector("body")};let e=null;t.start.addEventListener("click",(function(){e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.stop.disabled=!1,t.start.disabled=!0})),t.stop.addEventListener("click",(function(){clearInterval(e),t.start.disabled=!1,t.stop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.30adb52e.js.map
