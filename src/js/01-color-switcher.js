87eb02e8b5c3b90082cf92cfd72a55f543f7428f
Dima <dimasalabay0711@gmail.com>
2023-11-15 22:21:44 +0200

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+import '../css/common.css';
 
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerID = null;
+
+refs.start.addEventListener('click', startChanges);
+refs.stop.addEventListener('click', stopChanges);
+
+function startChanges() {
+  timerID = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.stop.disabled = false;
+  refs.start.disabled = true;
+}
+
+function stopChanges() {
+  clearInterval(timerID);
+
+  refs.start.disabled = false;
+  refs.stop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
